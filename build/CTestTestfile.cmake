1ee2637150185a713a499d134f77687aa9f7c747
lilei <37171564+xiaoleitongxue@users.noreply.github.com>
2022-07-29 22:04:45 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lilei/ros_workspace/src/subscriber_and_publisher
-# Build directory: /home/lilei/ros_workspace/src/subscriber_and_publisher/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
